f9e7f114ba971146e9e99b889c88c3a4b7b8a0e9
Brian Nguyen <briannoogin@yahoo.com>
2018-01-27 18:48:35 -0600

    Updates R script
    Made it a function so that an outside java function can use it

--- test.R
+++ test.R
@@ -1,10 +1,12 @@
+loadCSV <-function(searchKey,numTweets)
+{
 library(twitteR) #installs TwitteR library (twitteR) #loads TwitteR
-api_key <- "S9u9eLhjCKlyB2rKYB0Vm0ioA" #in the quotes, put your API key 
-api_secret <- "1pPtm5bwpFRuNpwX5WcQ8hBHjCQNgtsuKOAHygPDKqJ1Hcb3EL" #in the quotes, put your API secret token 
-token <- "957327348533284864-kClJhTjnirgiZMqxuM5KpPBvZkWyMrt" #in the quotes, put your token
-token_secret <- "FmlUTkRmaFOkwSlUWfptcL60QKgTbGeAOoLzEmSypNgwC" #in the quotes, put your token secret 
+api_key <- "FaVG24EilMLhWvE4aVFZT2ag9" #in the quotes, put your API key 
+api_secret <- "cNRHs7GsP6UbgNU8Aa3x6d58ZFqvtfBjDE6DwzJRl8opiz2pdT" #in the quotes, put your API secret token 
+token <- "957326291543515136-bMc0bb2H6VrSBEOwC9bYn8asAWEUBge" #in the quotes, put your token
+token_secret <- "JgKtiW8qe107ImrTMSMtRAbseA0HQMrSpvmrTTGlCx9lX" #in the quotes, put your token secret 
 setup_twitter_oauth(api_key, api_secret, token, token_secret)
-tweets <- searchTwitter("hurricane OR #hurricane OR harvey OR #hurricaneharvey", n = 3000, lang = "en")
-#tweets.df <-twListToDF(tweets)
-write.csv(twListToDF(tweets), "C:\\Users\\socce\\Documents\\TamuHackTest\\tweets.csv") #an example of a file extension of the folder in which you want to save the .csv file.
-
+tweets <- twListToDF(searchTwitter(searchKey, n = numTweets, lang = "en"))
+# windows running write.csv(twListToDF(tweets), "C:\\Users\\socce\\Documents\\TamuHackTest\\tweets.csv") #an example of a file extension of the folder in which you want to save the .csv file.
+write.csv(tweets, "tweets.csv") #an example of a file extension of the folder in which you want to save the .csv file.
+}
